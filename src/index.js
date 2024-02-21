1e861f3c68edc30a8aed3c741ea8363943f9bb0b
emmali98 <emma.m.li.98@gmail.com>
2024-02-21 13:39:37 -0500

    Form creates new TodoItem, prints to console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import TodoItem from './todoitem';
+
 function component() {
     const mainList = document.createElement('div');
     mainList.textContent = 'TODOs';
@@ -6,3 +8,17 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+// Click Handlers
+document.querySelector('form button').addEventListener("click", function(e) {
+    e.preventDefault();
+
+    let title = document.querySelector('#title').value;
+    let notes = document.querySelector('#notes').value;
+    let dueDate = document.querySelector('#due-date').value;
+
+    const newTodo = new TodoItem(title, notes, dueDate);
+    console.log(newTodo); // TODO: call render function instead
+
+    document.querySelector('form').reset();
+})
